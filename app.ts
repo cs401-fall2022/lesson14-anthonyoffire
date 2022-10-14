d769a06b0b691373d1c0ced58d132c081c1d5560
anthonyoffire <anthonybaird@protonmail.com>
2022-10-13 20:57:58 -0600

    CSS mostly done

--- app.ts
+++ app.ts
@@ -13,10 +13,13 @@ function hello (): String {
     if(document != null){
         let body = document.getElementById("demo");
         const hex = "0123456789ABCDEF";
-        let color = "#";
+        let color = "";
         for(let i=0; i<6; i++)
             color += hex[Math.floor(Math.random()*16)];
-        body.style.backgroundColor = color;
+        if(parseInt(color) % 4 == 0)
+            body.style.backgroundColor = "#f2f2e5";
+        else
+            body.style.backgroundColor = "#" + color;
     }    
 }
 
